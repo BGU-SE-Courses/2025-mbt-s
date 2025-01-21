099661dfd06c0e84c3a25ffad0f8dd02451d7eaf
yuvalBGU1 <avivyuv@post.bgu.ac.il>
2025-01-21 16:06:18 +0200

    setup selenium

---
+++ Cucumber/src/test/java/hellocucumber/SeleniumTest.java
@@ -0,0 +1,13 @@
+package hellocucumber;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SeleniumTest {
+
+    public static void main(String[] args) {
+        PrestashopActuator registration = new PrestashopActuator();
+        System.out.println("Success");
+    }
+}
