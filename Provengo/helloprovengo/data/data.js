960b66a7777d2101ad680b0a22d4466d5cc9da2a
NatallieMirelashvili <miralshv@post.bgu.ac.il>
2025-01-23 13:16:21 +0200

    Done implement base behavior.js and actions.js. We need to understand the test mechanism and implement test suits.

--- Provengo/helloprovengo/data/data.js
+++ Provengo/helloprovengo/data/data.js
@@ -10,16 +10,20 @@ const xpathUser = {
   email: '/html/body/main/section/div/div/div/section/div/section/form/div/div/div/input',
   password: '/html/body/main/section/div/div/div/section/div/section/form/div/div[2]/div/div/input',
   signInButton: '/html/body/main/section/div/div/div/section/div/section/form/footer/button',
-  product: '/html/body/main/section/div/div/div/section/section/section/div/div[2]/article/div/div/a/picture/img',
+  product: '/html/body/main/section/div/div/div/section/section/section/div/div[6]/article/div/div/a/picture/img',
   quantityCell: '/html/body/main/section/div/div/div/section/div/div[2]/div[2]/div[2]/form/div[2]/div/div/div/input',
-  addToCartButton: '/html/body/main/section/div/div/div/section/div/div[2]/div[2]/div[2]/form/div[2]/div/div[2]/button',
-  continueShoppingButton: '/html/body/div/div/div/div[2]/div/div[2]/div/div/a'
+  addToWishlistButton: '/html/body/main/section/div/div/div/section/div/div[2]/div[2]/div[2]/form/div[2]/div/button/i',
+  chooseMyWishlist: '/html/body/main/footer/div[2]/div/div/div[4]/div/div/div/div[2]/div/ul/li/p',
+  goToProfileButton: '/html/body/main/header/nav/div/div/div/div[2]/div[2]/div/a[2]/span',
+  wishlistButton: '/html/body/main/section/div/div/div/section/section/div/div/a[5]/span/i',
+  chooseList: '/html/body/main/section/div/div/div/section/div/section/div/ul/li/a/p'
 }
 
 const xpathAdmin = {
   email: '/html/body/div/div/div/div[3]/div/div/form/div/input',
   password: '/html/body/div/div/div/div[3]/div/div/form/div[2]/input',
   logInButton: '/html/body/div/div/div/div[3]/div/div/form/div[3]/button',
+  menu: '/html/body/header/nav/i',
   catalogButton: '/html/body/nav/div/ul/li[4]/a',
   productsButton: '/html/body/nav/div/ul/li[4]/ul/li/a',
   userProduct: '/html/body/div[2]/div/div[3]/div[2]/div/form[2]/table/tbody/tr/td[4]/a',
